cc6afd3b805d8548bd3cf7edb73bd011a23df441
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-12-08 15:07:41 +0100

    Create rainbow.bat

---
+++ rainbow.bat
@@ -0,0 +1,29 @@
+
+@echo_off
+
+cls
+
+echo Let's go! Click!
+
+pause
+
+:a
+
+color 0
+color 1
+color 2
+color 3
+color 4
+color 5
+color 6
+color 7
+color 8
+color 9
+color A
+color B
+color C
+color D
+color E
+color F
+
+goto :a
